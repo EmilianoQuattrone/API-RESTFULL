38b272575e3c2e229321a5de1db5792a3680a0f6
Emiliano Quattrone <emilianoquattrone008@gmail.com>
2024-11-12 14:01:52 -0300

    Se agrego otra extension, se configuro Data

---
+++ Data/AplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options)
+            : base (options) { }
+
+        // Crear los modelos o las entidades.
+        public DbSet<Category> Categories { get; set; }
+    }
+}
