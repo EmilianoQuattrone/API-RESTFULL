1faf2eaaa79b7a68922ba4b7992bb94b7fb93493
Emiliano Quattrone <emilianoquattrone008@gmail.com>
2024-11-12 12:55:19 -0300

    Creacion clase Category y sus propiedades

---
+++ Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Category
+    {
+        // Este decorador indica al campo que va hacer una PK y autoincremental.
+        [Key] 
+        public int Id { get; set; }
+
+        [Required]
+        public string NameCategory { get; set; }
+
+        [Required]
+        public DateTime CreationDate { get; set; }
+    }
+}
